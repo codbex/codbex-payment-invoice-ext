38eab35ce6b88f0279dcea3f4d931ddf0385d79f
StanZGenchev <stan.z.genchev@gmail.com>
2025-04-11 10:38:18 +0300

    Fixed dialog configs

--- codbex-payment-invoice-ext/generate/PurchaseInvoicePayment/purchase-invoice-payment-generate.js
+++ codbex-payment-invoice-ext/generate/PurchaseInvoicePayment/purchase-invoice-payment-generate.js
@@ -1,15 +1,12 @@
 const viewData = {
     id: 'purchase-invoice-payment-generate',
     label: 'Generate Purchase Invoice Payment',
-    link: '/services/web/codbex-payment-invoice-ext/generate/PurchaseInvoicePayment/generate-purchase-invoice-payment.html',
+    path: '/services/web/codbex-payment-invoice-ext/generate/PurchaseInvoicePayment/generate-purchase-invoice-payment.html',
     perspective: 'SupplierPayment',
     view: 'SupplierPayment',
     type: 'entity',
     order: 31
 };
-
 if (typeof exports !== 'undefined') {
-    exports.getDialogWindow = function () {
-        return viewData;
-    }
+	exports.getView = () => viewData;
 }
--- codbex-payment-invoice-ext/generate/SalesInvoicePayment/sales-invoice-payment-generate.js
+++ codbex-payment-invoice-ext/generate/SalesInvoicePayment/sales-invoice-payment-generate.js
@@ -1,15 +1,12 @@
 const viewData = {
     id: 'sales-invoice-payment-generate',
     label: 'Generate Sales Invoice Payment',
-    link: '/services/web/codbex-payment-invoice-ext/generate/SalesInvoicePayment/generate-sales-invoice-payment.html',
+    path: '/services/web/codbex-payment-invoice-ext/generate/SalesInvoicePayment/generate-sales-invoice-payment.html',
     perspective: 'CustomerPayment',
     view: 'CustomerPayment',
     type: 'entity',
     order: 31
 };
-
 if (typeof exports !== 'undefined') {
-    exports.getDialogWindow = function () {
-        return viewData;
-    }
+	exports.getView = () => viewData;
 }
